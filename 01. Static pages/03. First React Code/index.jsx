c5848dcef6c3e147da96525dafce90e044154b33
Daniel Buttig <daniel.buttig@gmail.com>
2025-09-29 21:36:19 -0500

    Lesson 03 - First React Code
    Modified the index.jsx to render html to the root div.

--- 01. Static pages/03. First React Code/index.jsx
+++ 01. Static pages/03. First React Code/index.jsx
@@ -2,3 +2,10 @@
 // You can render anything you want to the screen, doesn't have
 // to be an h1 element.
 
+import { createRoot } from "react-dom/client"
+// could instead import ReactDOM
+
+const root = createRoot(document.getElementById("root"))
+// if imported ReactDOM would instead call ReactDOM.createRoot
+
+root.render(<h1>Hello, React! I am Daniel!</h1>)
